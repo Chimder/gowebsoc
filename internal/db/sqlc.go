cfb7f1d644f64397e8c6f1839dcb9b3293041289
Chimder <dimas.kirilyuk@gmail.com>
2024-08-24 12:49:08 +0300

    QueryExecModeCacheDescribe

--- internal/db/sqlc.go
+++ internal/db/sqlc.go
@@ -5,6 +5,7 @@ import (
 	"goSql/internal/config"
 	"log"
 
+	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
 	_ "github.com/lib/pq"
 )
@@ -15,11 +16,11 @@ func DBConn(ctx context.Context) (*pgxpool.Pool, error) {
 		log.Fatalf("Unable to parse config: %v", err)
 		return nil, err
 	}
+	cfg.ConnConfig.DefaultQueryExecMode = pgx.QueryExecModeCacheDescribe
 	// cfg.AfterConnect = func(ctx context.Context, conn *pgx.Conn) error {
 	// 	_, err := conn.Exec(ctx, "DEALLOCATE ALL")
 	// 	return err
 	// }
-	// cfg.ConnConfig.RuntimeParams["prefer_simple_protocol"] = "1"
 
 	sqlcPool, err := pgxpool.NewWithConfig(ctx, cfg)
 	if err != nil {
